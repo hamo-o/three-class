54c8412b199f885b3045a1b3309e00fa2eec97e3
hamo-o <yheel369@gmail.com>
2022-11-11 06:32:32 +0900

    feat: 도형 추가 && 반응형 적용

--- src/index.js
+++ src/index.js
@@ -14,22 +14,53 @@ if (WEBGL.isWebGLAvailable()) {
     0.1,
     1000
   )
+  camera.position.z = 2
 
-  // 캔버스 DOMååå
+  // 캔버스 DOM
   const canvas = document.querySelector('#index')
 
   // 렌더러
-  const renderer = new THREE.WebGLRenderer({ canvas })
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
 
   // document.body.appendChild(renderer.domElement)
 
+  // 매쉬
+  const geometry01 = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+  const material01 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
+  const obj01 = new THREE.Mesh(geometry01, material01)
+  obj01.position.x = -1
+  scene.add(obj01)
+
+  const geometry02 = new THREE.ConeGeometry(0.4, 0.7, 6)
+  const material02 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
+  const obj02 = new THREE.Mesh(geometry02, material02)
+  scene.add(obj02)
+
+  const geometry03 = new THREE.IcosahedronGeometry(0.4, 0)
+  const material03 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
+  const obj03 = new THREE.Mesh(geometry03, material03)
+  obj03.position.x = 1
+  scene.add(obj03)
+
   // 렌더링 루프
   function render(time) {
-    time *= 0.001
+    time *= 0.0005
 
-    // cube.rotation.x = time
-    // cube.rotation.y = time
+    obj01.rotation.x = time
+    obj01.rotation.y = time
+
+    obj02.rotation.x = time
+    obj02.rotation.y = time
+
+    obj03.rotation.x = time
+    obj03.rotation.y = time
 
     renderer.render(scene, camera)
 
@@ -37,6 +68,15 @@ if (WEBGL.isWebGLAvailable()) {
   }
 
   requestAnimationFrame(render)
+
+  // 반응형 처리
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  window.addEventListener('resize', onWindowResize)
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
   document.body.appendChild(warning)
